7ccd3107b50fddee0bf7b888086adcb9a5e9bbc6
Bhagat Singh <bhagat.rawat@gmail.com>
2014-02-19 10:13:52 -0800

    Create RemovePidCallBack.java

---
+++ src/main/java/com/github/bhagatsingh/concurrency/process/RemovePidCallBack.java
@@ -0,0 +1,11 @@
+package com.github.bhagatsingh.concurrency.process;
+
+import java.util.List;
+/**
+ * 
+ * @author Bhagat Singh
+ *
+ */
+public interface RemovePidCallBack {
+    public void processResult(List<Integer> pids); 
+}
